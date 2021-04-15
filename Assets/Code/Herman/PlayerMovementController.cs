4e00aa6b4344cade5802b535d6e2bf54b0b9f3f2
Herman Braggs <42748017+BraggsHerman@users.noreply.github.com>
2021-04-14 22:38:22 -0500

    mad changes to tiles and things

--- Assets/Code/Herman/PlayerMovementController.cs
+++ Assets/Code/Herman/PlayerMovementController.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     public float speed = 3.0f;
+    Rigidbody2D rb;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
+    private void FixedUpdate()
+    {
         Vector2 position = transform.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
         transform.position = position;
 
-
-
+        rb.MovePosition(position);
     }
 }

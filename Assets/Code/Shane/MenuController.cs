49b4197d2081a5a9282c333f15c1f5f1552d84b0
scanf13ld <scanfield413@gmail.com>
2021-04-25 22:42:58 -0500

    menu linking on pause

--- Assets/Code/Shane/MenuController.cs
+++ Assets/Code/Shane/MenuController.cs
@@ -40,8 +40,6 @@ namespace test
         {
             //Turn off all menus
             mainMenu.SetActive(false);
-            optionsMenu.SetActive(false);
-            levelMenu.SetActive(false);
 
             //Turn on requested menu
             someMenu.SetActive(true);
@@ -52,29 +50,16 @@ namespace test
         {
             SwitchMenu(mainMenu);
         }
-        public void ShowOptionsMenu()
+
+        public void Exit()
         {
-            SwitchMenu(optionsMenu);
-        }
-        public void ShowLevelMenu()
-        {
-            SwitchMenu(levelMenu);
+            SceneManager.LoadScene("Main");
+
         }
 
-        public void ResetScore()
-        {
-            PlayerPrefs.DeleteKey("Score");
-            PlayerController.instance.score = 0;
-        }
 
-        public void LoadLevel()
-        {
-            SceneManager.LoadScene("DungeonPerson");
-        }
         //Outlets
         public GameObject mainMenu;
-        public GameObject optionsMenu;
-        public GameObject levelMenu;
 
         // Start is called before the first frame update
         void Start()
